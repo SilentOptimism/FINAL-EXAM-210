04a061fac9c40001ddd8f7e6e74240138269c166
Joey Dhillon <imjoeydhillon@gmail.com>
2024-12-09 18:46:55 -0800

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
-
 using namespace std;
+
+#include <chrono>
+using namespace std::chrono;
+
+struct node{
+    string customerName;
+    string product;
+};
+
+class linkedList(){
+private:
+    Node* head = NULL;
+
+};
+
+
+
 
 int main(int argc, char const *argv[])
 {
-    cout << "test" << endl;
+    int round = 0;
+
+    time_point start = high_resolution_clock::now();
+    time_point now = high_resolution_clock::now();
+
+    while (round <= 10){
+        time_point now = high_resolution_clock::now();
+
+        milliseconds duration = duration_cast<milliseconds>(now-start);
+
+        if(duration.count() >1000){
+
+            now = high_resolution_clock::now();
+            round++;
+        }
+
+    }
+    
+    
     return 0;
 }
