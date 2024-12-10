742e5420d2b622eb3954b7aec443f5a6c9b2837a
Joey Dhillon <imjoeydhillon@gmail.com>
2024-12-09 18:56:25 -0800

    commit 3

--- main.cpp
+++ main.cpp
@@ -4,17 +4,37 @@ using namespace std;
 #include <chrono>
 using namespace std::chrono;
 
-struct node{
+struct Node{
     string customerName;
     string product;
+    Node* next = NULL;
+
+    Node(string name, string item) : customerName(name), product(item) {}
+    
 };
 
-class linkedList(){
+class CoffeeBooth{
 private:
     Node* head = NULL;
 
+public:
+    void push(string customerName, string product){
+        Node* curr = head;
+
+        while (curr != NULL){
+            curr = curr->next;
+        }
+
+        curr = new Node(customerName, product);
+    }
 };
 
+string name[50] = {
+
+};
+string drink[50] ={
+
+};
 
 
 
@@ -24,13 +44,21 @@ int main(int argc, char const *argv[])
 
     time_point start = high_resolution_clock::now();
     time_point now = high_resolution_clock::now();
+    CoffeeBooth cafe;
 
     while (round <= 10){
         time_point now = high_resolution_clock::now();
 
         milliseconds duration = duration_cast<milliseconds>(now-start);
 
-        if(duration.count() >1000){
+        if(duration.count() > 1000){
+            // ACTUAL SIMULATION CODE HERE 
+            cafe.push("asdf", "erth");
+            cafe.push("asdf", "erth");
+            cafe.push("asdf", "erth");
+            
+
+
 
             now = high_resolution_clock::now();
             round++;
